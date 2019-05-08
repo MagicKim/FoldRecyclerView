01f467cba0f0edfffe40435387c77bc03130e182
zetaojin <zetaojin@ecarx.com.cn>
2019-05-08 20:07:50 +0800

    feat:添加聚合的注释
    Signed-off-by: zetaojin <zetaojin@ecarx.com.cn>

--- app/src/main/java/com/chad/baserecyclerviewadapterhelper/adapter/ExpandableItemAdapter.java
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/adapter/ExpandableItemAdapter.java
@@ -346,8 +346,6 @@ public class ExpandableItemAdapter extends BaseMultiItemQuickAdapter<MultiItemEn
     }
 
 
-    private ArrayList<TestNotification> notificationArrayList = new ArrayList<>();
-
     //聚合转换
 /*    public void addGroupItem(TestNotification notification) {
         notificationArrayList.add(notification);
@@ -384,12 +382,18 @@ public class ExpandableItemAdapter extends BaseMultiItemQuickAdapter<MultiItemEn
         setNewData(entityList);
     }*/
 
+    //原始数据
+    private ArrayList<TestNotification> notificationArrayList = new ArrayList<>();
+    //转换的数据
     private ArrayList<MultiItemEntity> entityList = new ArrayList<>();
 
     //聚合转换
     public void addGroupItem(TestNotification xcnRecord) {
+        //添加原始数据
         notificationArrayList.add(xcnRecord);
+        //构建时间集合
         ArrayList<Long> childTimeList = new ArrayList<>();
+        //使用map分组
         Map<String, List<TestNotification>> resultMap = new LinkedHashMap<>();
         for (TestNotification record : notificationArrayList) {
             String pkg = record.getPkg();
@@ -401,34 +405,36 @@ public class ExpandableItemAdapter extends BaseMultiItemQuickAdapter<MultiItemEn
                 resultMap.put(pkg, tmp);
             }
         }
+        //同一包名下,2条消息才会聚合
         if (resultMap.get(xcnRecord.getPkg()).size() < 2) {
-            Log.e(TAG, "添加默认的布局 同一个包名下面的消息小于2 " + xcnRecord.getPkg());
+            Log.e(TAG, "添加默认的布局 同一个包名下面的第一条消息不聚合 " + xcnRecord.getPkg());
             NormalItem normalItem = new NormalItem(xcnRecord.getTitle(), xcnRecord.getContent(), xcnRecord.getPkg());
             normalItem.setTime(xcnRecord.getTime());
             entityList.add(normalItem);
         } else {
-            Log.e(TAG, "聚合布局------>" + xcnRecord.getPkg());
+            Log.e(TAG, "开始聚合布局------>" + xcnRecord.getPkg());
             Iterator<MultiItemEntity> iterator = getData().iterator();
             while (iterator.hasNext()) {
                 MultiItemEntity next = iterator.next();
                 if (itemType(next) == TYPE_NORMAL) {
                     NormalItem normalItem = (NormalItem) next;
                     if (normalItem.getPackageName().equals(xcnRecord.getPkg())) {
-                        Log.e(TAG, "聚合布局,需要删除原来默认的数据 " + xcnRecord.getPkg());
+                        Log.e(TAG, "聚合布局,需要删除视图集合中normal类型的数据 " + xcnRecord.getPkg());
                         iterator.remove();
                     }
                 }
             }
-//            Log.e(TAG, "notificationList = " + notificationList.size());
+            //遍历map集合
             for (String key : resultMap.keySet()) {
                 Log.e(TAG, "遍历map");
+                //如果遍历的key 与刚刚来的通知的package是一样的才聚合
                 if (TextUtils.equals(key, xcnRecord.getPkg())) {
-                    Log.e(TAG, " getData() = "+getData().size() );
+                    //首先删除视图 getData()里面的数据,防止重复添加
                     Iterator<MultiItemEntity> iteratorGroup = getData().iterator();
                     while (iteratorGroup.hasNext()) {
                         MultiItemEntity next = iteratorGroup.next();
                         if (itemType(next) == TYPE_LEVEL_0) {
-                            Log.e(TAG, " TYPE_LEVEL_0" );
+                            Log.e(TAG, " TYPE_LEVEL_0");
                             Level0Item level0Item = (Level0Item) next;
                             if (level0Item.getPackageName().equals(xcnRecord.getPkg())) {
                                 Log.e(TAG, "聚合布局,需要删除原来已经聚合的内容 " + xcnRecord.getPkg());
@@ -437,12 +443,14 @@ public class ExpandableItemAdapter extends BaseMultiItemQuickAdapter<MultiItemEn
                         }
                     }
                     Log.e(TAG, "如果这条消息与map中的key值一样,那么聚合");
+                    //添加聚合实体类
                     List<TestNotification> notificationList = resultMap.get(key);
-                    Level0Item foldParentItem = new Level0Item(xcnRecord.getTitle(),xcnRecord.getPkg());
+                    Level0Item foldParentItem = new Level0Item(xcnRecord.getTitle(), xcnRecord.getPkg());
                     for (TestNotification timeNotification : notificationList) {
                         childTimeList.add(timeNotification.getTime());
                         foldParentItem.addSubItem(new Level1Item(timeNotification.getTitle(), timeNotification.getPkg(), true, timeNotification.getContent()));
                     }
+                    //排序
                     Long maxTime = Collections.max(childTimeList);
                     foldParentItem.time = maxTime;
                     foldParentItem.title = xcnRecord.getPkg();
@@ -451,9 +459,13 @@ public class ExpandableItemAdapter extends BaseMultiItemQuickAdapter<MultiItemEn
                 }
             }
         }
+        //将转换的集合加入视图需要的数据源getData
         getData().addAll(entityList);
+        //排序
         Collections.sort(getData(), mGroupEntityCmp);
+        //通知刷新UI
         setNewData(getData());
+        //清空数据转换集合
         entityList.clear();
     }
 
